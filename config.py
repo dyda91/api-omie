2a356210081d1e17acae4ed06bf2a0caa9df8471
dyda91 <edjamasalmeida@outlook.com>
2024-01-09 19:10:48 -0300

    atualiza variaveis de ambiente

--- config.py
+++ config.py
@@ -16,16 +16,16 @@ CORS(app)
 
 app_key = os.getenv('APP_KEY')
 app_secret = os.getenv('APP_SECRET') 
+conexao = os.getenv('SQLALCHEMY_DATABASE_URI') 
 bcrypt = Bcrypt(app)
 
 login_manager = LoginManager(app)
 
 db.init_app(app)
-# conexao = 'mysql://root:BbdF6G5G6g665e6D3-BeHAAb5-C4hHe3@mysql.railway.internal:3306/railway'
-
+# conexao = 'sqlite:///database.db'
 
 migrate = Migrate(app, db)
 
 app.config['SECRET_KEY'] = 'my-secret-key'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:BbdF6G5G6g665e6D3-BeHAAb5-C4hHe3@monorail.proxy.rlwy.net:29587/railway'
+app.config['SQLALCHEMY_DATABASE_URI'] = conexao
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
