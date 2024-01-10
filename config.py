4086c79fa1cba5578099a9fa41120e9c0343f28f
dyda91 <edjamasalmeida@outlook.com>
2024-01-09 21:00:09 -0300

    add

--- config.py
+++ config.py
@@ -1,42 +1,3 @@
-from flask_sqlalchemy import SQLAlchemy
-from flask import Flask
-from flask_migrate import Migrate
-from flask_login import LoginManager
-from flask_bcrypt import Bcrypt
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-app = Flask(__name__)
-db = SQLAlchemy(app)
-
-app_key = os.getenv('APP_KEY')
-app_secret = os.getenv('APP_SECRET')
-conexao = os.getenv('SQLALCHEMY_DATABASE_URI')
-
-# conexao = "mysql://root:BbdF6G5G6g665e6D3-BeHAAb5-C4hHe3@monorail.proxy.rlwy.net:29587/railway"
-app.config['SECRET_KEY'] = 'my-secret-key'
-app.config['SQLALCHEMY_DATABASE_URI'] = conexao
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
-db.init_app(app)
-migrate = Migrate(app, db)
-
-
-
-
-
-
-
-
-
-
-
-
 # from flask_sqlalchemy import SQLAlchemy
 # from flask import Flask
 # from flask_migrate import Migrate
@@ -45,26 +6,65 @@ migrate = Migrate(app, db)
 # import os
 # from dotenv import load_dotenv
 
-# from flask_cors import CORS
-
 # load_dotenv()
 
-# app = Flask (__name__)
+# app = Flask(__name__)
 # db = SQLAlchemy(app)
-# CORS(app)
 
 # app_key = os.getenv('APP_KEY')
-# app_secret = os.getenv('APP_SECRET') 
-# conexao = os.getenv('SQLALCHEMY_DATABASE_URI') 
-# bcrypt = Bcrypt(app)
+# app_secret = os.getenv('APP_SECRET')
+# conexao = os.getenv('SQLALCHEMY_DATABASE_URI')
 
-# login_manager = LoginManager(app)
-
-# db.init_app(app)
-# # conexao = 'sqlite:///database.db'
-
-# migrate = Migrate(app, db)
-
+# # conexao = "mysql://root:BbdF6G5G6g665e6D3-BeHAAb5-C4hHe3@monorail.proxy.rlwy.net:29587/railway"
 # app.config['SECRET_KEY'] = 'my-secret-key'
 # app.config['SQLALCHEMY_DATABASE_URI'] = conexao
 # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+# bcrypt = Bcrypt(app)
+# login_manager = LoginManager(app)
+# db.init_app(app)
+# migrate = Migrate(app, db)
+
+
+
+
+
+
+
+
+
+
+
+
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
+from flask_migrate import Migrate
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
+import os
+from dotenv import load_dotenv
+
+from flask_cors import CORS
+
+load_dotenv()
+
+app = Flask (__name__)
+db = SQLAlchemy(app)
+CORS(app)
+
+app_key = os.getenv('APP_KEY')
+app_secret = os.getenv('APP_SECRET') 
+conexao = os.getenv('SQLALCHEMY_DATABASE_URI') 
+bcrypt = Bcrypt(app)
+
+login_manager = LoginManager(app)
+
+db.init_app(app)
+conexao = 'sqlite:///database.db'
+
+migrate = Migrate(app, db)
+
+app.config['SECRET_KEY'] = 'my-secret-key'
+app.config['SQLALCHEMY_DATABASE_URI'] = conexao
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
